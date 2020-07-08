7ce72fcac1274c544080a19bbedff3380a35217b
aureliowozhiak <aureliowozhiak@gmail.com>
2020-07-08 19:42:12 -0300

    Some HTML edits and firebase connection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using My_Portfolio_system.Models;
+using RestSharp;
 
 namespace My_Portfolio_system.Controllers
 {
     public class HomeController : Controller
     {
+
+        private RestClient client = new RestClient("https://portfolio-aureliowozhiak.firebaseio.com/portfolio.json");
+        
+        
+
+        public IActionResult Index()
+        {
+
+            var request = new RestRequest("", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            var model = JsonSerializer.Deserialize<List<Portfolio>>(response.Content);
+
+            return View(model);
+        }
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -18,15 +37,6 @@ namespace My_Portfolio_system.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
